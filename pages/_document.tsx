224803715c4b4259f4b7bcc701794108492b73c9
bicz <bicz87@gmail.com>
2020-02-19 11:01:19 +0100

    add FB event tracking pixel (#36)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,8 @@ import Document, {
 import { ServerStyleSheet } from 'styled-components'
 import { GA_TRACKING_ID } from '../lib/gtag'
 
+const FB_TRACKING_ID = '213264439836530'
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
@@ -54,6 +56,23 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window,document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+               fbq('init', '${FB_TRACKING_ID}');
+              fbq('track', 'PageView');            
+              `
+            }}
+          />
+
         </Head>
         <body>
           <Main />
